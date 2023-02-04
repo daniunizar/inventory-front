b71de872dbd8567474723bef741bda9042c4f867
Daniel Marcos <daniunizar@gmail.com>
2023-02-04 12:43:24 +0100

    relocalte Login and fix import error

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
+import LoginView from '../components/views/no-auth/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
